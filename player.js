7f8cc4905528cd07d23f63e04e8b4f4b5178e45a
moji29nabe <moji29nabe@localhost>
2015-06-08 00:25:33 +0900

    不要なコードを削除

--- player.js
+++ player.js
@@ -1,21 +1,10 @@
 var Player = function(id){
   this.id = id;
-  this.x = 0;
-  this.y = 0;
-  this.keys = {
-    up : false,
-    down : false,
-    right : false,
-    left : false
-  };
   this.te = null;
   this.result = null;
 };
 
 Player.prototype = {
-  updateKey: function(key, status){
-    this.keys[key] = status;
-  },
   updateTe: function(te){
     this.te = te;
   }
