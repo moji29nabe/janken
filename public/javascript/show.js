71cac0378ce3a3fcdf05afae4450591a118bdc60
moji29nabe <moji29nabe@localhost>
2015-07-06 22:38:31 +0900

    リファクタリング(不要なファイル・箇所を削除。ライブラリはminを使用)

--- public/javascript/show.js
+++
@@ -1,9 +0,0 @@
-// var map = new MoonMap('#new_players', {
-//   moonSelector: '.moonSelector',
-//   radius: 140
-// });
-
-// var map = new MoonMap('#center', {
-//   moonSelector: '.moonSelector',
-//   radius: 140
-// });
